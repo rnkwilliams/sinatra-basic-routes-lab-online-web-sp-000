38736e015030b95334b83accf62ffa0ef79f9941
Ranika Williams <rnkprice07@gmail.com>
2019-09-04 17:45:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
   get('/name') {"My name is_"}
   get('/hometown') {"My hometown is _"}
   get('/favorite-song') {"My favorite song is _"}
